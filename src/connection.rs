1f4ac040a01aa9e562fb8668f5a5c929eec33f12
judegiordano <judegiordano@gmail.com>
2024-05-30 10:56:20 -0400

    expose regex

--- src/connection.rs
+++ src/connection.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use async_once::AsyncOnce;
 use lazy_static::lazy_static;
 use mongodb::{options::ClientOptions, Client, Database};
@@ -13,13 +15,15 @@ lazy_static! {
             "mongodb://localhost:27017/mongoose-rs-local".to_string(),
             |uri| uri,
         );
-        let client_options = ClientOptions::parse(mongo_uri).await.map_or_else(
+        let mut client_options = ClientOptions::parse(mongo_uri).await.map_or_else(
             |err| {
                 tracing::error!("error parsing client options {:?}", err);
                 std::process::exit(1);
             },
             |opts| opts,
         );
+        client_options.max_pool_size = Some(500);
+        client_options.connect_timeout = Some(Duration::from_secs(10));
         let client = Client::with_options(client_options).map_or_else(
             |err| {
                 tracing::error!("error connecting client: {:?}", err);
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 // expose 3rd party crates
 pub use bson::doc;
-pub use mongodb::{options::IndexOptions, IndexModel};
+pub use mongodb::{bson::Regex, options::IndexOptions, IndexModel};
 
 // feature exports
 #[cfg(feature = "uuid")]
